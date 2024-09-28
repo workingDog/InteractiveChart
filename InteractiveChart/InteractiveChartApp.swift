429284f5bae1e83de35d420e9538ab79799eea69
workingdog <ringowathelet@gmail.com>
2024-09-29 08:19:56 +0900

    Initial Commit

---
+++ InteractiveChart/InteractiveChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveChartApp.swift
+//  InteractiveChart
+//
+//  Created by Ringo Wathelet on 2024/09/29.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
